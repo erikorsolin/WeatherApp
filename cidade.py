a490a2b649ec29781f45ce482715ee2cd164cf04
Erik Orsolin <depaulaerik670@gmail.com>
2023-02-17 15:49:51 -0300

    Correção de Bugs

--- cidade.py
+++ cidade.py
@@ -12,23 +12,22 @@ class Cidade:
         self.speedVento = 0
 
     
-
     #Set
     def call_api(self):
         chave = "abdcb2f05a1704c2ef9a456e893a3f41"
         link = f"https://api.openweathermap.org/data/2.5/weather?q={self.nome}&appid={chave}&lang=pt_br"
         requisicao = requests.get(link)
         dic = requisicao.json()
-
+        
         self.descricao = dic['weather'][0]['description']
-        self.temperatura = dic['main']['temp'] - 273.15
-        self.sensacao = dic['main']['feels_like']
-        self.minima = dic['main']['temp_min'] - 273.15
-        self.maxima = dic['main']['temp_max'] - 273.15
+        self.temperatura = '{:.2f}'.format(dic['main']['temp'] - 273.15)
+        self.sensacao = '{:.2f}'.format(dic['main']['feels_like']- 273.15)
+        self.minima = '{:.2f}'.format(dic['main']['temp_min'] - 273.15)
+        self.maxima = '{:.2f}'.format(dic['main']['temp_max'] - 273.15)
         self.pressao = dic['main']['pressure']
         self.umidade = dic['main']['humidity']
-        self.speedVento = dic['wind']['speed'] * 3.6
+        self.speedVento = '{:.2f}'.format(dic['wind']['speed'] * 3.6)
 
 
-    
-    
+
+
--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 from cidade import Cidade
-while True:
-    nome = input('Digite o nome da cidade que deseja obter as condições meteorológicas: ')
+from tkinter import *
+
+def main():
+    nome = entrada.get()
     c1 = Cidade(nome)
-    c1.call_api()
-    print('Descrição: {}'.format(c1.descricao))
-    print('Temperatura: {:.2f} C'.format(c1.temperatura))
-    print('Sensação térmica: {:.2f} C'.format(c1.sensacao))
-    print('Temperatura Mínima: {:.2f} C'.format(c1.minima))
-    print('Temperatura Máxima: {:.2f} C'.format(c1.maxima))
-    print('Pressão Atmosférica: {}"Hg'.format(c1.pressao))
-    print('Umidade: {}'.format(c1.umidade))
-    print('Velocidade do vento: {:.2f} KM/h'.format(c1.speedVento))
+    try:
+        c1.call_api()
 
-    w = input('Encerrar Programa[S/N]: ').upper()
-    if w == "N":
-        break
+        texto = f'''
+        Descrição: {c1.descricao}
+        Umidade: {c1.umidade}
+        Temperatura: {c1.temperatura} C
+        Sensação térmica: {c1.sensacao} C
+        Temperatura Mínima: {c1.minima} C
+        Temperatura Máxima: {c1.maxima} C
+        Pressão Atmosférica: {c1.pressao}"Hg    
+        Velocidade do vento: {c1.speedVento} KM/h'''
+        texto_condicoes['text'] = texto
+    except:
+        texto_condicoes['text'] = 'Cidade não encontrada :('
 
+
+
+interface = Tk()
+interface.title('Weather App')
+interface.geometry('500x400')
+interface['bg'] = "#5C7394"
+msg = Label(interface, text='Condições meteorológicas atualizadas', font='Times 15', bg='#5C7394')
+msg.grid(column=0, row=0, padx=15, pady=15)
+
+txt = Label(interface, text='Digite o nome da cidade ', font= 'Arial 9 italic', bg='#5C7394')
+txt.grid(column=0, row=1, padx=10, pady=10)
+entrada = Entry(interface, width=27)
+entrada.grid(column=0, row=3, padx=10, pady=10)
+
+
+botao = Button(interface, text='Obter',command=main, bg='#447BC7')
+botao.grid(column=1, row=3, padx=15, pady=15)
+
+    
+texto_condicoes = Label(interface, text='', bg='#5C7394', font='Arial 12')
+texto_condicoes.grid(column=0, row=4, padx=15, pady=15)
+
+interface.mainloop()
